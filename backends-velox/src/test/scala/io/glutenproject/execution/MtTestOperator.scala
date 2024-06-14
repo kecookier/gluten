8612252774c7d82823346d474b8288fc7364cd4b
zhaokuo <zhaokuo_game@163.com>
2024-06-14 10:33:38 +0800

    Add MtTestOperator

---
+++ backends-velox/src/test/scala/io/glutenproject/execution/MtTestOperator.scala
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.glutenproject.execution
+
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{AnalysisException, DataFrame, Row}
+import org.apache.spark.sql.execution.adaptive.AdaptiveSparkPlanHelper
+
+class MtTestOperator extends VeloxWholeStageTransformerSuite with AdaptiveSparkPlanHelper {
+
+  protected val rootPath: String = getClass.getResource("/").getPath
+  override protected val backend: String = "velox"
+  override protected val resourcePath: String = "/tpch-data-parquet-velox"
+  override protected val fileFormat: String = "parquet"
+
+  import testImplicits._
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    createTPCHNotNullTables()
+  }
+
+  override protected def sparkConf: SparkConf = {
+    super.sparkConf
+      .set("spark.shuffle.manager", "org.apache.spark.shuffle.sort.ColumnarShuffleManager")
+      .set("spark.sql.files.maxPartitionBytes", "1g")
+      .set("spark.sql.shuffle.partitions", "1")
+      .set("spark.memory.offHeap.size", "2g")
+      .set("spark.unsafe.exceptionOnMemoryLeak", "true")
+      .set("spark.sql.autoBroadcastJoinThreshold", "-1")
+      .set("spark.sql.sources.useV1SourceList", "avro")
+  }
+
+  private def checkFallbackOperators(df: DataFrame, num: Int): Unit = {
+    // Decrease one VeloxColumnarToRowExec for the top level node
+    assert(
+      collect(df.queryExecution.executedPlan) {
+        case p if p.isInstanceOf[VeloxColumnarToRowExec] => p
+      }.size - 1 == num,
+      df.queryExecution)
+  }
+
+  test("Support multi-children count with row construct") {
+    runQueryAndCompare(
+      """
+        |select l_orderkey, count(distinct l_partkey, l_comment), corr(l_partkey, l_partkey+1)
+        |from lineitem group by l_orderkey
+        |""".stripMargin
+    )(df => checkFallbackOperators(df, 0))
+  }
+}
