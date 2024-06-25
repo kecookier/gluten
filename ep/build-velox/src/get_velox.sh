05d75113ceaa50f743384e9bbef933ed0fddf9d0
zhaokuo <zhaokuo_game@163.com>
2024-06-25 19:50:20 +0800

    update velox branch

--- ep/build-velox/src/get_velox.sh
+++ ep/build-velox/src/get_velox.sh
@@ -16,8 +16,8 @@
 
 set -exu
 
-VELOX_REPO=https://github.com/oap-project/velox.git
-VELOX_BRANCH=2024_06_24
+VELOX_REPO=git@github.com:kecookier/gluten.git
+VELOX_BRANCH=2024_06_24_mt
 VELOX_HOME=""
 
 #Set on run gluten on HDFS
